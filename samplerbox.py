193c8669d4d6ca9cae280dc0db46c8f0f5b48819
Mojca Miklavec <mojca.miklavec.lists@gmail.com>
2022-07-28 16:59:45 +0200

    Make samplerbox.py work with rtmidi-python

--- samplerbox.py
+++ samplerbox.py
@@ -23,7 +23,7 @@ import sounddevice
 import threading
 from chunk import Chunk
 import struct
-import rtmidi_python as rtmidi
+import rtmidi
 import samplerbox_audio
 
 #########################################
@@ -424,14 +424,14 @@ if USE_SYSTEMLED:
 # MAIN LOOP
 #########################################
 
-midi_in = [rtmidi.MidiIn(b'in')]
+midi_in = [rtmidi.MidiIn()]
 previous = []
 while True:
-    for port in midi_in[0].ports:
+    for port in midi_in[0].get_ports():
         if port not in previous and b'Midi Through' not in port:
-            midi_in.append(rtmidi.MidiIn(b'in'))
+            midi_in.append(rtmidi.MidiIn())
             midi_in[-1].callback = MidiCallback
             midi_in[-1].open_port(port)
             print('Opened MIDI: ' + str(port))
-    previous = midi_in[0].ports
+    previous = midi_in[0].get_ports()
     time.sleep(2)
