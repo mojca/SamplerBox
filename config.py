07028ef56a7912c0637a25e38292e5ed0cd0603a
Mojca Miklavec <mojca.miklavec.lists@gmail.com>
2023-01-04 01:31:30 +0100

    Adjust directory for reading samples, use serial in

--- config.py
+++ config.py
@@ -4,11 +4,11 @@
 #########################################
 
 AUDIO_DEVICE_ID = 2                     # change this number to use another soundcard
-SAMPLES_DIR = "."                       # The root directory containing the sample-sets. Example: "/media/" to look for samples on a USB stick / SD card
+SAMPLES_DIR = "/media/"                 # The root directory containing the sample-sets. Example: "/media/" to look for samples on a USB stick / SD card
 MAX_POLYPHONY = 80                      # This can be set higher, but 80 is a safe value
 USE_BUTTONS = False                     # Set to True to use momentary buttons (connected to RaspberryPi's GPIO pins) to change preset
 USE_I2C_7SEGMENTDISPLAY = False         # Set to True to use a 7-segment display via I2C
-USE_SERIALPORT_MIDI = False             # Set to True to enable MIDI IN via SerialPort (e.g. RaspberryPi's GPIO UART pins)
+USE_SERIALPORT_MIDI = True              # Set to True to enable MIDI IN via SerialPort (e.g. RaspberryPi's GPIO UART pins)
 USE_SYSTEMLED = False                   # Flashing LED after successful boot, only works on RPi/Linux
 SERIALPORT_PORT = '/dev/ttyUSB0'
 SERIALPORT_BAUDRATE = 115200
